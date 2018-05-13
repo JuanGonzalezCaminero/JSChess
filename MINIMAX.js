435912823fe3dc41324820c7006443049f0fbc5c
Juan <juangonzalez5@hotmail.es>
2018-05-13 04:54:56 +0200

    AI pretty much working

--- MINIMAX.js
+++ MINIMAX.js
@@ -9,6 +9,7 @@ Arguments:
 	-node: Current node
 	-player: MIN or MAX
 	-ply: how many levels to explore in advance from this one
+	-initialNode: Whether this is the first node of the exploration
 Returns: 
 	-The best option among the children of the given node, or an 
 	array with equally valid options
@@ -16,10 +17,9 @@ Returns:
 //const MIN = 0;
 //const MAX = 1;
 
-function minimax(node, player, ply){
+function minimax(node, player, ply, initialNode){
 	let children = new Array(0);
 	let heuristicValues = new Array(0);
-	let nextPlayer;
 	let options = new Array(0);
 
 	if(player == MIN){
@@ -31,56 +31,86 @@ function minimax(node, player, ply){
 	if(ply != 0){
 		children = getChildren(node);
 		for(let i = 0; i < children.length; i++){
-			heuristicValues.push(minimax(children[i], nextPlayer, ply - 1));
+			heuristicValues.push(minimax(children[i], nextPlayer, ply - 1, false));
 		}
-		//Get the index of the node with the Larger or Smaller value
-		//depending on the current player
-		if(player == MIN){
-			//Get the index of the node with the Smaller heuristic value
-			//Getting the Smaller value
-			let min = heuristicValues.reduce(function(previousVal, currentVal, index, array) {
-				//previousVal is the last returned value
-				if(currentVal < previousVal){
-					return currentVal;
-				}else{
-					return previousVal;
+		if(initialNode){
+			//Get the index of the node with the Larger or Smaller value
+			//depending on the current player
+			if(player == MIN){
+				//Get the index of the node with the Smaller heuristic value
+				//Getting the Smaller value
+				let min = heuristicValues.reduce(function(previousVal, currentVal, index, array) {
+					//previousVal is the last returned value
+					if(currentVal < previousVal){
+						return currentVal;
+					}else{
+						return previousVal;
+					}
+				});
+				//Search for all the nodes that share the same heuristic value,
+				//if there are more than one, and return them
+				for(let index = 0; index < heuristicValues.length; index++){
+					if(heuristicValues[index] == min){
+						options.push(children[index]);
+					}
 				}
-			});
-			//Search for all the nodes that share the same heuristic value,
-			//if there are more than one, and return them
-			for(let index = 0; index < heuristicValues.length; index++){
-				if(heuristicValues[index] == min){
-					options.push(children[index]);
-				}
-			}
-			return options;
-		}else{
-			//Get the index of the node with the Larger heuristic value
-			//Getting the Larger value
-			let max = heuristicValues.reduce(function(previousVal, currentVal, index, array) {
-				//previousVal is the last returned value
-				if(currentVal > previousVal){
-					return currentVal;
-				}else{
-					return previousVal;
-				}
-			});
-			//If there are several nodes with the same value, only the first one
-			//is returned
-			//console.log("HValues: ", heuristicValues);
-			//console.log("Max: ", max);
-			//console.log("Index of max: ", heuristicValues.indexOf(max));
-			//console.log("Chosen node: ", children[heuristicValues.indexOf(max)]);
-			//console.log("Children: ", children.length, ", HValues: ", heuristicValues.length);
+				return options;
+			}else{
+				//Get the index of the node with the Larger heuristic value
+				//Getting the Larger value
+				let max = heuristicValues.reduce(function(previousVal, currentVal, index, array) {
+					//previousVal is the last returned value
+					if(currentVal > previousVal){
+						return currentVal;
+					}else{
+						return previousVal;
+					}
+				});
+				//If there are several nodes with the same value, only the first one
+				//is returned
+				//console.log("HValues: ", heuristicValues);
+				//console.log("Max: ", max);
+				//console.log("Index of max: ", heuristicValues.indexOf(max));
+				//console.log("Chosen node: ", children[heuristicValues.indexOf(max)]);
+				//console.log("Children: ", children.length, ", HValues: ", heuristicValues.length);
 
-			//Search for all the nodes that share the same heuristic value,
-			//if there are more than one, and return them
-			for(let index = 0; index < heuristicValues.length; index++){
-				if(heuristicValues[index] == max){
-					options.push(children[index]);
+				//Search for all the nodes that share the same heuristic value,
+				//if there are more than one, and return them
+				for(let index = 0; index < heuristicValues.length; index++){
+					if(heuristicValues[index] == max){
+						options.push(children[index]);
+					}
 				}
+				return options;
 			}
-			return options;
+		}else{
+			//Get the index of the node with the Larger or Smaller value
+			//depending on the current player
+			if(player == MIN){
+				//Get the index of the node with the Smaller heuristic value
+				//Getting the Smaller value
+				let min = heuristicValues.reduce(function(previousVal, currentVal, index, array) {
+					//previousVal is the last returned value
+					if(currentVal < previousVal){
+						return currentVal;
+					}else{
+						return previousVal;
+					}
+				});
+				return min;
+			}else{
+				//Get the index of the node with the Larger heuristic value
+				//Getting the Larger value
+				let max = heuristicValues.reduce(function(previousVal, currentVal, index, array) {
+					//previousVal is the last returned value
+					if(currentVal > previousVal){
+						return currentVal;
+					}else{
+						return previousVal;
+					}
+				});
+				return max;
+			}
 		}
 	}else{
 		return(heuristicValue(node));
